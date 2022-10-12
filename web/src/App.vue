6d5d1436e704892d764d8f5fb25a9c9aa0807a56
pspapleng <chayanin3343@gmail.com>
2022-10-12 23:27:32 +0700

    update: use navbar and footer

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,3 +1,19 @@
 <template>
-  <router-view />
+  <div>
+    <NavBar />
+    <router-view />
+    <Footer />
+  </div>
 </template>
+<script>
+import { defineComponent } from "vue";
+import NavBar from "./components/NavBar.vue";
+import Footer from "./components/Footer.vue";
+
+export default defineComponent({
+  components: {
+    NavBar,
+    Footer,
+  },
+});
+</script>
