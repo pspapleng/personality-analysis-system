4338db8c7d455c0b08a18a5474cd423a7c906f7c
pspapleng <chayanin3343@gmail.com>
2022-12-18 12:11:37 +0700

    fix: ignore insecure ssl

--- web/src/providers/axios.js
+++ web/src/providers/axios.js
@@ -2,6 +2,9 @@ import axios from "axios";
 
 const httpClient = axios.create({
   baseURL: "https://me-dev.trueddns.com:59771",
+  httpsAgent: new https.Agent({
+    rejectUnauthorized: false,
+  }),
 });
 
 export { httpClient };
