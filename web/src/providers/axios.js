0cc04a5bec4d939212f77725c889bca3bc201211
pspapleng <chayanin3343@gmail.com>
2022-12-18 12:07:50 +0700

    fix: change server

--- web/src/providers/axios.js
+++ web/src/providers/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const httpClient = axios.create({
-  baseURL: "http://me-dev.trueddns.com:59771",
+  baseURL: "https://me-dev.trueddns.com:59771",
 });
 
 export { httpClient };
